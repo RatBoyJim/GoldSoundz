8768778f1a51e0bf810251736ad50d04dc9a7bfc
Michael Anderson <ratboyjim@gmail.com>
2020-11-10 17:34:34 +0000

    main page styling started

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template
 from controllers.album_controller import albums_blueprint
 from controllers.label_controller import labels_blueprint
 
+from models.album import Album
+from models.label import Label
+import repositories.album_repository as album_repository
+import repositories.label_repository as label_repository
+
 
 app = Flask(__name__)
 
@@ -12,7 +17,8 @@ app.register_blueprint(labels_blueprint)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    albums = album_repository.select_all()
+    return render_template('index.html', albums = albums)
 
 if __name__ == '__main__':
     app.run(debug=True)
